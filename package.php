5ee775765da8b9bbccfd9d51813e4c6e15e278de
Dave Ryan <0aveRyan@users.noreply.github.com>
2020-04-30 08:25:41 -0700

    push up

---
+++ package.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * ☁️ PressNitro WordPress Replicators
+ * * *
+ * 🖨 Replicate new WordPress Plugins, Themes, WP-CLI Packages and features using WP-CLI and Mustache Templates.
+ * * *
+ *
+ * 1. 👀 Make sure WP-CLI is available
+ * 2. 🔍 Make sure autoloader file is available.
+ * 3. ✅ Autoload \\PressNitro\\Replicate files.
+ * 4. 🛠 Setup 'wp replicate' WP-CLI Commands
+ */
+
+/**
+ * 1. 👀 Make sure WP-CLI is available
+ */
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+/**
+ * 2. 🔍 Make sure autoloader file is available.
+ */
+if ( ! is_readable( $replicate_command_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	return;
+}
+
+/**
+ * 3. ✅ Autoload \\PressNitro\\Replicate files.
+ */
+require_once $replicate_command_autoloader;
+
+/**
+ * 4. 🏗 Setup 'wp replicate' WP-CLI Commands
+ */
+$namespace = '\\PressNitro\\Replicate\\';
+
+$commands  = [
+	'plugin' => 'Plugin'
+];
+
+foreach( $commands as $cmd => $class ) {
+	WP_CLI::add_command( 
+		sprintf( 'replicate %s', $cmd ), 
+		$namespace . $class . '\\Command' 
+	);
+}
+
+// WP_CLI::add_command( 'replicate install', '\\PressNitro\\Replicate\\Install' );
+// WP_CLI::add_command( 'replicate package', '\\PressNitro\\Replicate\\Package' );
+// WP_CLI::add_command(
+// 	'replicate playground',
+// 	$namespace . 'Playground\\Command'
+// );
+
+// WP_CLI::add_command(
+// 	'replicate plugin',
+// 	$namespace . 'Plugin\\Command'
+// );
+// WP_CLI::add_command( 'replicate setup', '\\PressCloud\\Replicate\\Setup' );
+// WP_CLI::add_command( 'replicate theme', '\\PressCloud\\Replicate\\Theme' );
---
+++ src/Plugin/Command.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PressNitro\Replicate\Plugin;
+
+use \PressNitro\Replicator\Common;
+use \WP_CLI;
+
+/**
+ * 🔌 Replicate WordPress Plugins from Boilerplate
+ */
+class Command extends Common {
+	/**
+	 * Function Invoked by WP-CLI to Replicate Plugins.
+	 *
+	 * @param array $args - WP-CLI Positional Arguments
+	 * @param array $assoc_args - WP-CLI Associative Arguments & Flags
+	 * @return void
+	 */
+	public function __invoke( $args, $assoc_args ) {
+		$this->initialConfig();
+		$this->commonPrompts( $args, $assoc_args );
+		// do any custom prompts here
+		// $this->setupStructure();
+
+		$this->runReplication();
+	}
+
+	/**
+	 * This is a good opportunity to set required attributes:
+	 * - $this->type (valid strings include: 'plugin', 'theme', 'package')
+	 * - $this->template (path to directory containing mustache templates)
+	 * 
+	 * @return void
+	 */
+	protected function initialConfig() {
+		$this->type      = 'plugin';
+		$this->templates = __DIR__ . '/templates';
+	}
+
+	protected function setupStructure() {
+		$this->structure = [
+			"{$this->data['slug']}.php" => 'plugin.php',
+			'README.md'  				=> 'README.md',
+		];
+	}
+}
